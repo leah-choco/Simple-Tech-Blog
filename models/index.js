bbaf87ff0b88db2df07ec2b45d90b8498edc37ec
Your Name <you@example.com>
2022-11-02 14:01:12 -0500

    Creating index.js

---
+++ models/index.js
@@ -0,0 +1,13 @@
+/*const User = require('./User');
+const newName = require('./newName');
+
+User.hasMany(newName, {
+    foreignKey: "user_id",
+    onDelete: 'CASCADE'
+});
+
+//newname.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, newName };*/
