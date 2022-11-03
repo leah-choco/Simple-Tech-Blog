6c9a3dfaf5259915170923a4dd909c1fc1cdd3b7
Your Name <you@example.com>
2022-11-03 13:20:45 -0500

    Imported comments to index

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, BlogPost } = require('../models');
+const { User, BlogPost, Comments } = require('../models');
 
 const userData = require('./userData.json');
 const blogData = require('./blogData.json');
+const commentData = require("./comment");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
