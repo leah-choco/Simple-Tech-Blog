b13bf3a5f2b7ee38c2cb5ec9c1afd70a56b542bf
Your Name <you@example.com>
2022-11-07 14:29:45 -0600

    Creating more handlebars files

--- seeds/seed.js
+++ seeds/seed.js
@@ -5,22 +5,6 @@ const userData = require('./userData.json');
 const blogData = require('./blogData.json');
 const commentData = require("./commentData.json");
 
-const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
 
-  for (const blog of blogData) {
-    await BlogPost.create({
-      ...blog,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
 
-  process.exit(0);
-};
-
-seedDatabase();
