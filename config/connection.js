7d6b33dc56df9750e64dde11368c4e2c23f7b1b6
Your Name <you@example.com>
2022-11-17 12:33:29 -0600

    Changing connection file for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-const Sequelize = require('sequelize');
+/*const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
@@ -9,5 +9,30 @@ const sequelize = process.env.JAWSDB_URL
       port: 3306
     });
 
-module.exports = sequelize;
+module.exports = sequelize;*/
 
+// import Sequelize constructor from the library
+const Sequelize = require("sequelize");
+
+// import dotenv for password security
+require("dotenv").config();
+
+// create connection to our database, pass in MySql info for username/password
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
+
+module.exports = sequelize;
