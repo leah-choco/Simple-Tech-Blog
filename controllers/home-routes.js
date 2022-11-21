4c997af2b15d8471672dd98bf983f8528607c24c
Your Name <you@example.com>
2022-11-21 15:51:43 -0600

    Adding more get routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,7 +6,7 @@ const withAuth = require("../utils/auth");
 router.get("/", async (req, res) => {
   try {
     res.render("homepage", {
-      logged_in: req.session.loggedIn,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -15,7 +15,7 @@ router.get("/", async (req, res) => {
 
 //Getting login page
 router.get("/login", (req, res) => {
-  if (req.session.loggedIn) {
+  if (req.session.logged_in) {
     res.redirect("/dashboard");
     return;
   }
@@ -24,12 +24,37 @@ router.get("/login", (req, res) => {
 
 //Getting signup page
 router.get("/signup", (req, res) => {
-  if (req.session.loggedIn) {
+  if (req.session.logged_in) {
     res.redirect("/dashboard");
     return;
   }
 
   res.render("sign-up");
+});
+
+//Get the new blog post form
+router.get("/blog", (req, res) => {
+  if (!req.session.logged_in) {
+    res.sendStatus(404);
+    return;
+  }
+  res.render("blog");
+});
+
+//Get the edit blog form
+router.get("edit/:id", async (req, res) => {
+  try {
+    const blogsData = await BlogPost.findByPk(req.params.id);
+
+    const blog = blogsData.get({ plain: true });
+
+    res.render("editpost", {
+      blog: blog,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
